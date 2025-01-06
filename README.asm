18d97c7ab8dffa858cf4817b1ba49e2de9c7d4e1
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2025-01-05 18:32:06 -0800

    Create README.asm

---
+++ README.asm
@@ -0,0 +1,8 @@
+; Sear-c is a library to make a search engine using the work given.
+; It is a simple framework designed to those people who want to make their own search engine and know C/C++ to modify this.
+; This is composed of four parts: HTML, CSS, JS, and a C file to allow people to modify this search engine.
+; Thus, we can do whatever modifications we want -  whether it is to give this search engine a browser, or something other than that!
+; So use Sear-c to modify and clay your very own search engine by forking this repo.
+; This is why you should use Sear-c to do this.
+; Thank you,
+; @akishore15
